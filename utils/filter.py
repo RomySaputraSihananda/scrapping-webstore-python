b7e4bc65ac6405bb74a7860243d77ce034e9656e
romys <romys.1222@gmail.com>
2023-08-30 23:14:05 +0700

    add penjelasan setiap baris code

--- utils/filter.py
+++ utils/filter.py
@@ -1,29 +1,63 @@
+# import module BeautifulSoup dari library bs4 (analisis dan pemrosesan HTML)
 from bs4 import BeautifulSoup;
 
+# declaration function filter dengan parameter html
 def filter(html):
 
+    # membuat object BeautifulSoup dengan argument html dari parameter dan 'html.parser' bawaan dari BeautifulSoup
     soup = BeautifulSoup(html, 'html.parser');
+
+    # assignment list kosong ke variable data
     data = [];
+
+    # mencari semua elemen `<div class='col-xs-2-4 shopee-search-item-result__item'>` (card product) dan diassign ke variable cards
     cards = soup.find_all('div', attrs={'class': 'col-xs-2-4 shopee-search-item-result__item'});
 
+    # validasi jika elemen yang ditemukan, maka function akan mereturn nilai False
     if(not len(cards)): return False;
 
+    # looping setiap card pada variable cards
     for card in cards:
-        
+
+        # mencari elemen `<div class='_1yN94N WoKSjC _2KkMCe'/>` (nama product), mengambil text-nya dan diassign ke variable nama
         nama = card.find('div', attrs={'class': '_1yN94N WoKSjC _2KkMCe'}).get_text();
+
+        # mencari elemen `<a/>` (link product), mengambil href-nya dan diassign ke variable link
         link = card.find('a').get('href');
+
+        # mencari elemen `<div class='cbl0HO MUmBjS'/>` (harga product), mengambil text-nya dan diassign ke variable harga
         harga = card.find('div', attrs={'class': 'cbl0HO MUmBjS'}).get_text();
+
+        # mencari elemen `<img class='B0Ze3i wAkToc'/>` (image product), mengambil src-nya dan diassign ke variable img
         img = card.find('img', attrs={'class': 'B0Ze3i wAkToc'}).get('src');
+
+        # mencari elemen `<div class='mrz-bA'>` (kota product), mengambil text-nya dan diassign ke variable kota
         kota = card.find('div', attrs={'class': 'mrz-bA'}).get_text();
+
+        # mencari elemen `<div class='x+3B8m wOebCz'>` (product terjual), mengambil text-nya dan diassign ke variable terjual
         terjual = card.find('div', attrs={'class': 'x+3B8m wOebCz'}).get_text();
 
+        # menambah dictionary ke variable data
         data.append({
+
+            # membuat key nama dengan value variable nama
             'nama': nama,
+
+            # membuat key link dengan value https://shopee.co.id + variable link
             'link': f'https://shopee.co.id{link}',
+
+            # membuat key harga dengan value variable harga
             'harga': harga,
+
+            # membuat key img dengan value variable img
             'img': img,
+
+            # membuat key kota dengan value variable kota
             'kota': kota,
+
+            # membuat key terjual dengan value variable terjual
             'terjual': terjual,
         });
     
+    # mereturn data (berisi kumpulan data semua card)
     return data;
--- utils/write.py
+++ utils/write.py
@@ -1,25 +1,45 @@
+# import modul JSON (memformat list ke JSON)
 import json;
+
+# import library Pandas sebagai pd (menulis data ke file xlsx dan csv)
 import pandas as pd;
+
+# import module os (validasi dan menulis sebuah directory)
 import os; 
 
+# declaration function write dengan parameter data, category, name
 def write(data, category, name):
+
+    # Mengubah data dari parameter menjadi format JSON
     data = json.dumps(data);
 
+    # menentukan directory penyimpanan berdasarkan category dari parameter 
     path = f'data/{category}';
 
+    # mengecek directory sudah ada apa belum 
     isExist = os.path.exists(path);
+
+    # validasi jika variable isExist bernilai False, maka dibuatkan directory dari variable path
     if(not isExist): os.makedirs(path);
-        
+    
+    # menulis variable data dengan format JSON ke file JSON dengan nama name + .json 
     with open(f'{path}/{name}.json', 'w') as file:
         file.write(data);
     
+    # membaca variable data dengan format JSON sebagai DataFrame menggunakan Pandas
     df = pd.read_json(data);
     
+    # menyimpan DataFrame ke dalam file csv dengan nama name + .csv
     df.to_csv(f'{path}/{name}.csv');
 
+    # merename label index DataFrame menjadi no
     df.index = df.index.rename('no');
+
+    # menambahkan 1 ke setiap index
     df.index = df.index + 1;
 
+    # menyimpan DataFrame ke dalam file excel dengan nama name + .xlsx
     df.to_excel(f'{path}/{name}.xlsx');
     
+    # log ke layar path dari result data yang telah ditulis 
     print(f'Result   : {path}\n');
