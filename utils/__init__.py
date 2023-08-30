0a6119d7d7c7b12e1d80d67b57d790ea2ad902b5
romys <romys.1222@gmail.com>
2023-08-30 23:33:47 +0700

    add penjelasan setiap baris code

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,8 @@
+# import function browser dari module browser
 from .browser import browser; 
+
+# import function filter dari module filter
 from .filter import filter; 
+
+# import function write dari module write
 from .write import write; 
